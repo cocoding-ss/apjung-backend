3da2e13bd4fd17faec4145a305a0c5baa145ebed
labyu <labyu2020@gmail.com>
2020-09-12 00:54:51 +0900

    readme 추가

--- api/src/main/java/me/wiro/backend/WiromeBackendApplication.java
+++ api/src/main/java/me/wiro/backend/WiromeBackendApplication.java
@@ -1,12 +1,12 @@
-package me.wiro.api;
+package me.wiro.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WiromeApiApplication {
+public class WiromeBackendApplication {
 
     public static void main(String [] args) {
-        SpringApplication.run(WiromeApiApplication.class, args);
+        SpringApplication.run(WiromeBackendApplication.class, args);
     }
 }
