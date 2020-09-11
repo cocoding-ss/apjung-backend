eba40c568a1d7e4b2ae5c67e24890b8323dfdbea
labyu <labyu2020@gmail.com>
2020-09-11 23:59:21 +0900

    Spring Boot Application 생성

---
+++ api/src/main/java/me/wiro/api/WiromeApiApplication.java
@@ -0,0 +1,12 @@
+package me.wiro.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiromeApiApplication {
+
+    public static void main(String [] args) {
+        SpringApplication.run(WiromeApiApplication.class, args);
+    }
+}
