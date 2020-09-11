43638aa8875ab0eee16d27b3d736b850bf9c1967
labyu <labyu2020@gmail.com>
2020-09-12 00:57:04 +0900

    commit

---
+++ api/src/main/java/me/wiro/backend/api/TestController.java
@@ -0,0 +1,13 @@
+package me.wiro.backend.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String test() {
+        return "TEST v1";
+    }
+}
