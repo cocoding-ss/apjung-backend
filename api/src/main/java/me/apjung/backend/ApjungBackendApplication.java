ce5c4c2036770da858da601dd9c139cf159b3ea9
labyu <labyu2020@gmail.com>
2020-09-13 20:47:03 +0900

    commit

--- api/src/main/java/me/apjung/backend/ApjungBackendApplication.java
+++ api/src/main/java/me/apjung/backend/ApjungBackendApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApjungBackendApplication {
 
+
+
+
     public static void main(String [] args) {
         SpringApplication.run(ApjungBackendApplication.class, args);
     }
