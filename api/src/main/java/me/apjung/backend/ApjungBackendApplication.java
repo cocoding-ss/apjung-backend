c7e48a781777c0eca36a87aa82bd7b3ca268bc85
labyu <labyu2020@gmail.com>
2020-09-16 16:43:59 +0900

    test

--- api/src/main/java/me/apjung/backend/ApjungBackendApplication.java
+++ api/src/main/java/me/apjung/backend/ApjungBackendApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApjungBackendApplication {
 
     public static void main(String [] args) {
-
-
-
         SpringApplication.run(ApjungBackendApplication.class, args);
     }
 }
