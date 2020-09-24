3427c75a6faf2b29854a3f976464c52f6c13ab53
labyu <labyu2020@gmail.com>
2020-09-24 16:30:44 +0900

    resources 추가

--- api/src/main/java/me/apjung/backend/api/HealthController.java
+++ api/src/main/java/me/apjung/backend/api/HealthController.java
@@ -1,13 +1,17 @@
 package me.apjung.backend.api;
 
+import me.apjung.backend.property.MyProfile;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
+    @Autowired
+    private MyProfile myProfile;
 
     @GetMapping("/health")
     public String test() {
-        return "v15";
+        return "v15 " + myProfile.getDefaultProfile() + " " + myProfile.getMyProfile();
     }
 }
