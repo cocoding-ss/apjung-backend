b8fd899bf9b03126a91c2af7fb3ee72a8d43ac63
labyu <labyu2020@gmail.com>
2020-10-03 02:05:58 +0900

    스프링 시큐리티 설정을 Yaml로 분리, 또한 스프링 시큐리티 설정시 해당 프로퍼티를 이용하도록 변경함

--- api/src/test/java/me/apjung/backend/property/AppPropsTest.java
+++ api/src/test/java/me/apjung/backend/property/AppPropsTest.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class CustomAppPropsTest {
+public class AppPropsTest {
     @Autowired
     AppProps appProps;
 
