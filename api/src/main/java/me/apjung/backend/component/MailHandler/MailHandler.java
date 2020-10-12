d19631db8f9ac37e806f233cf145880be1ffd34b
labyu <labyu2020@gmail.com>
2020-10-12 21:54:30 +0900

    Mail Handler와 MailMessage를 implement할 CustomMailMessage 작성. 또한 이후의 개발환경에서의 잘못된 메일 전송을 방지하기 위해 devEmails라는 프로퍼티를 추가하고 sendMail()함수에서 처리하도록 함

--- api/src/main/java/me/apjung/backend/component/MailHandler/MailHandler.java
+++ api/src/main/java/me/apjung/backend/component/MailHandler/MailHandler.java
@@ -6,6 +6,9 @@ import org.springframework.mail.MailParseException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -15,30 +18,43 @@ import java.util.Arrays;
 public class MailHandler {
     private final JavaMailSender sender;
     private final AppProps appProps;
+    private final String mailTemplatePrefix = "templates/email/";
 
     public MailHandler(JavaMailSender sender, AppProps appProps) {
         this.sender = sender;
         this.appProps = appProps;
     }
 
-    public void sendMail(CustomMailMessage message) throws MessagingException {
+    public void send(CustomMailMessage message) throws MessagingException {
         this.authorizeEnvMessageTo(message.getTo());
 
         MimeMessage mimeMessage = sender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
 
         helper.setSubject(message.getSubject());
-        helper.setText(message.getText());
+        helper.setText(message.getText(), message.isHtml());
         helper.setFrom(message.getFrom());
         helper.setTo(message.getTo());
 
         sender.send(mimeMessage);
     }
 
+    public String getTemplateHtml(String templatePath, Context context) {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+
+        templateResolver.setPrefix(this.mailTemplatePrefix);
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+        templateEngine.setTemplateResolver(templateResolver);
+
+        return templateEngine.process(templatePath, context);
+    }
+
     private void authorizeEnvMessageTo(String to) throws MailParseException {
         if (!AppEnv.PROD.equals(appProps.getCurrentEnv())) {
             if (Arrays.stream(appProps.getDevEmails().toArray()).noneMatch(to::equals)) {
-                throw new MailParseException("개발 환경에서는 특정된 이메일에만 이메일으 전송할 수 있습니다");
+                throw new MailParseException("개발 환경에서는 특정된 이메일에만 이메일을 전송할 수 있습니다");
             }
         }
     }
--- api/src/test/java/me/apjung/backend/component/MailHandler/MailHandlerTest.java
+++ api/src/test/java/me/apjung/backend/component/MailHandler/MailHandlerTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.thymeleaf.context.Context;
 
 import javax.mail.MessagingException;
 
@@ -17,15 +18,24 @@ public class MailHandlerTest {
     public void 메일_전송() throws MessagingException {
         try {
             // given
+            Context context = new Context();
+            context.setVariable("title", "This is Test Title");
+            context.setVariable("content", "This is <p> content");
+
+            String text = mailHandler.getTemplateHtml("test", context);
+
+            // when
             CustomMailMessage message = CustomMailMessage.builder()
                     .from("admin@apjung.me")
                     .to("labyu2020@gmail.com")
-                    .subject("Text Subject")
-                    .text("Test Message")
-                    .isHtml(false)
+                    .subject("This is Test Email")
+                    .text(text)
+                    .isHtml(true)
                     .build();
 
-            mailHandler.sendMail(message);
+            mailHandler.send(message);
+
+            // then not exception
         } catch (Exception e) {
             e.printStackTrace();
             throw e;
