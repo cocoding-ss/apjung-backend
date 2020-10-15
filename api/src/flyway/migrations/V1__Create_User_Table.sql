c314351997bd6e3c11953030ea92530c8946e25b
labyu <labyu2020@gmail.com>
2020-10-15 22:21:55 +0900

    flyway 해결 못함

---
+++ api/src/flyway/migrations/V1__Create_User_Table.sql
@@ -0,0 +1,11 @@
+drop table user if exists;
+CREATE TABLE `user` (
+  `user_id` bigint NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `email_auth_token` varchar(255) DEFAULT NULL,
+  `is_email_auth` bit(1) NOT NULL,
+  `mobile` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+)
