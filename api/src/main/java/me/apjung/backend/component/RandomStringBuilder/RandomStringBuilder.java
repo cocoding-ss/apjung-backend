f1bc54c6bea4659c6f48f1440fb5c871853abfcb
labyu <labyu2020@gmail.com>
2020-11-28 22:15:36 +0900

    SplittableRandom 객체로 변경

--- api/src/main/java/me/apjung/backend/component/RandomStringBuilder/RandomStringBuilder.java
+++ api/src/main/java/me/apjung/backend/component/RandomStringBuilder/RandomStringBuilder.java
@@ -1,15 +1,17 @@
 package me.apjung.backend.component.RandomStringBuilder;
 
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.util.Random;
+import java.util.SplittableRandom;
 
 public class RandomStringBuilder {
     public static String  generateAlphaNumeric(int length) {
         int leftLimit = '0';
         int rightLimit = 'z';
 
-//            Random random = SecureRandom.getInstanceStrong();
-        return "helheroofw234";
+        SplittableRandom random = new SplittableRandom();
+        return random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || 65 <= i) && (i <= 90 || 97 <= i))
+                .limit(length)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
     }
 }
