fa8ff304117a86c5aee3b99bfad55f50be441f42
YoungWukJeon <kdh10080@gmail.com>
2020-12-09 03:24:57 +0900

    data.sql을 제거하고 @BeforeEach로 데이터 테스트할 수 있게 리포지터리 테스트 변경

--- api/src/test/java/me/apjung/backend/AbstractDataJpaTest.java
+++ api/src/test/java/me/apjung/backend/AbstractDataJpaTest.java
@@ -1,6 +1,5 @@
 package me.apjung.backend;
 
-import lombok.RequiredArgsConstructor;
 import me.apjung.backend.config.Jpa.JpaConfig;
 import me.apjung.backend.config.QueryDslConfig;
 import org.junit.jupiter.api.Disabled;
@@ -12,7 +11,6 @@ import org.springframework.context.annotation.Import;
 @DataJpaTest
 @Import({QueryDslConfig.class, JpaConfig.class})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@RequiredArgsConstructor
 @Disabled
 public abstract class AbstractDataJpaTest {
     protected TestEntityManager testEntityManager;
--- api/src/test/java/me/apjung/backend/repository/shop/ShopRepositoryTest.java
+++ api/src/test/java/me/apjung/backend/repository/shop/ShopRepositoryTest.java
@@ -1,9 +1,17 @@
 package me.apjung.backend.repository.shop;
 
 import me.apjung.backend.AbstractDataJpaTest;
+import me.apjung.backend.domain.Base.ViewStats;
+import me.apjung.backend.domain.File.File;
+import me.apjung.backend.domain.User.User;
+import me.apjung.backend.domain.shop.Shop;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.data.domain.PageRequest;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,28 +20,100 @@ public class ShopRepositoryTest extends AbstractDataJpaTest {
 
     @BeforeEach
     public void setUpData() {
+        final var user = User.builder()
+                .email("testuser@gmail.com")
+                .emailAuthToken("1YPsW4mdDQz7ctPvcwGDaTMgcU7eehkK9Y3QzLWOq6QSduudE44SNhlLb6JC")
+                .isEmailAuth(false)
+                .mobile("01012341234")
+                .name("iamtest")
+                .password("$2a$10$hkWoqKEJTBqadPUuv9W/FO/UOdxEBc2ekaHf6JcLOmhCOgT7hwbiq")
+                .build();
 
+        final var file = File.builder()
+                .prefix("LOCAL/public/")
+                .name("pBhvKVzqU8vhYi76Kb8Xgm78xU1w54VaVI8JkWv4weXiuh0pkyFAnSm9g7gB.jpg")
+                .extension("jpg")
+                .originalName("440x440.jpg")
+                .originalExtension("jpg")
+                .publicUrl("https://storage.apjung.xyz/LOCAL/public/pBhvKVzqU8vhYi76Kb8Xgm78xU1w54VaVI8JkWv4weXiuh0pkyFAnSm9g7gB.jpg")
+                .size(20705L)
+                .width(440)
+                .height(440)
+                .isImage(false)
+                .build();
 
-        testEntityManager.persistFlushFind();
+        final var shops = List.of(
+                Shop.builder()
+                        .thumbnail(file)
+                        .name("test shop")
+                        .url("https://www.naver.com")
+                        .overview("테스트 쇼핑몰입니다1")
+                        .viewStats(new ViewStats(0L, 0L))
+                        .build(),
+                Shop.builder()
+                        .name("무신sa TOAST")
+                        .url("https://www.naver.com")
+                        .overview("테스트 쇼핑몰입니다2")
+                        .viewStats(new ViewStats(8L, 2L))
+                        .build(),
+                Shop.builder()
+                        .name("test shop2")
+                        .url("https://www.naver.com")
+                        .overview("테스트 쇼핑몰입니다3")
+                        .viewStats(new ViewStats(8L, 4L))
+                        .build(),
+                Shop.builder()
+                        .name("4XR test")
+                        .url("https://www.naver.com")
+                        .overview("테스트 쇼핑몰입니다4")
+                        .viewStats(new ViewStats(8L, 6L))
+                        .build(),
+                Shop.builder()
+                        .name("12 무신사 test shop")
+                        .url("https://www.naver.com")
+                        .overview("테스트 쇼핑몰입니다5")
+                        .viewStats(new ViewStats(6L, 6L))
+                        .build());
+
+        testEntityManager.persist(user);
+        testEntityManager.persist(file);
+        shops.forEach(s -> testEntityManager.persist(s));
     }
 
     @Autowired
-    public ShopRepositoryTest(ShopRepository shopRepository) {
+    public ShopRepositoryTest(TestEntityManager testEntityManager, ShopRepository shopRepository) {
+        super.testEntityManager = testEntityManager;
         this.shopRepository = shopRepository;
     }
 
     @Test
     @DisplayName("최신 등록일로 정렬된 쇼핑몰 리스트 조회")
     public void findAllByOrderByCreatedAtDescTest() {
-        shopRepository.findAllByOrderByCreatedAtDesc(PageRequest.of(0, 10))
-                .forEach(e -> System.out.println("id: " + e.getId() + ", name: '" + e.getName() + "', createdAt: " + e.getCreatedAt()));
+        final var expected = List.of(
+                "테스트 쇼핑몰입니다5", "테스트 쇼핑몰입니다4", "테스트 쇼핑몰입니다3", "테스트 쇼핑몰입니다2", "테스트 쇼핑몰입니다1");
+
+        final var result = shopRepository.findAllByOrderByCreatedAtDesc(PageRequest.of(0, 10))
+                .stream()
+                .peek(e -> System.out.println("id: " + e.getId() + ", name: '" + e.getName() + "' overview: '" + e.getOverview() +"', createdAt: " + e.getCreatedAt()))
+                .map(Shop::getOverview)
+                .collect(Collectors.toList());
+
+        assertIterableEquals(expected, result);
     }
 
     @Test
     @DisplayName("이름으로 정렬(가나다순)된 쇼핑몰 리스트 조회")
     public void findAllByOrderByNameTest() {
-        shopRepository.findAllByOrderByName(PageRequest.of(0, 10))
-                .forEach(e -> System.out.println("id: " + e.getId() + ", name: '" + e.getName() + "'"));
+        final var expected = List.of(
+                "테스트 쇼핑몰입니다5", "테스트 쇼핑몰입니다4", "테스트 쇼핑몰입니다1", "테스트 쇼핑몰입니다3", "테스트 쇼핑몰입니다2");
+
+        final var result = shopRepository.findAllByOrderByName(PageRequest.of(0, 10))
+                .stream()
+                .peek(e -> System.out.println("id: " + e.getId() + ", name: '" + e.getName() + "'"))
+                .map(Shop::getOverview)
+                .collect(Collectors.toList());
+
+        assertIterableEquals(expected, result);
     }
 
     @Test
@@ -41,8 +121,16 @@ public class ShopRepositoryTest extends AbstractDataJpaTest {
     @Disabled
     @Deprecated
     public void findAllByNameIgnoreCaseContainingOrderByCreatedAtDescTest() {
-        shopRepository.findAllByNameIgnoreCaseContainingOrderByCreatedAtDesc("test", PageRequest.of(0, 10))
-                .forEach(e -> System.out.println("id: " + e.getId() + ", name: '" + e.getName() + "', createdAt: " + e.getCreatedAt()));
+        final var expected = List.of(
+                "테스트 쇼핑몰입니다5", "테스트 쇼핑몰입니다4", "테스트 쇼핑몰입니다3", "테스트 쇼핑몰입니다1");
+
+        final var result = shopRepository.findAllByNameIgnoreCaseContainingOrderByCreatedAtDesc("test", PageRequest.of(0, 10))
+                .stream()
+                .peek(e -> System.out.println("id: " + e.getId() + ", name: '" + e.getName() + "', createdAt: " + e.getCreatedAt()))
+                .map(Shop::getOverview)
+                .collect(Collectors.toList());
+
+        assertIterableEquals(expected, result);
     }
 
     @Test
@@ -50,8 +138,16 @@ public class ShopRepositoryTest extends AbstractDataJpaTest {
     @Disabled
     @Deprecated
     public void findAllByNameIgnoreCaseContainingOrderByNameTest() {
-        shopRepository.findAllByNameIgnoreCaseContainingOrderByName("무신", PageRequest.of(0, 10))
-                .forEach(e -> System.out.println("id: " + e.getId() + ", name: '" + e.getName() + "'"));
+        final var expected = List.of(
+                "테스트 쇼핑몰입니다5", "테스트 쇼핑몰입니다2");
+
+        final var result = shopRepository.findAllByNameIgnoreCaseContainingOrderByName("무신", PageRequest.of(0, 10))
+                .stream()
+                .peek(e -> System.out.println("id: " + e.getId() + ", name: '" + e.getName() + "'"))
+                .map(Shop::getOverview)
+                .collect(Collectors.toList());
+
+        assertIterableEquals(expected, result);
     }
 
     @Test
