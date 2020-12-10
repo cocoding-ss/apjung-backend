c8d85b562293b4c58b90eed752d44bf6805f37e9
YoungWukJeon <kdh10080@gmail.com>
2020-12-11 05:09:22 +0900

    스키마 변경 및 생성

--- api/src/main/resources/db/migration/V20201211042828__Create_Shops_View_Stats_Table.sql
+++ api/src/main/resources/db/migration/V20201211042828__Create_Shops_View_Stats_Table.sql
@@ -3,7 +3,9 @@ use `apjung`;
 DROP TABLE IF EXISTS `shops_view_stats`;
 CREATE TABLE `shops_view_stats`
 (
+    `shop_view_stats_id` bigint NOT NULL,
     `shop_id` bigint NOT NULL,
     `page_view` bigint NOT NULL DEFAUlt 0,
-    `unique_visitor` bigint NOT NULL DEFAUlt 0
+    `unique_visitor` bigint NOT NULL DEFAUlt 0,
+    PRIMARY KEY (`shop_view_stats_id`)
 );
