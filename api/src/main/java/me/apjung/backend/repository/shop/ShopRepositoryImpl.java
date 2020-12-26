ec7c109366caaafb080ff0e6f813e9e37074a8a0
YoungWukJeon <kdh10080@gmail.com>
2020-12-26 22:22:37 +0900

    Querydsl 작업 중...

--- api/src/main/java/me/apjung/backend/repository/shop/ShopRepositoryImpl.java
+++ api/src/main/java/me/apjung/backend/repository/shop/ShopRepositoryImpl.java
@@ -1,22 +1,27 @@
 package me.apjung.backend.repository.shop;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import me.apjung.backend.domain.shop.QShop;
-import me.apjung.backend.domain.shop.QShopTag;
-import me.apjung.backend.domain.shop.Shop;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.apjung.backend.domain.shop.*;
 import me.apjung.backend.domain.tag.QTag;
+import me.apjung.backend.domain.tag.Tag;
+import me.apjung.backend.dto.projections.ShopSearchResult;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.thymeleaf.util.StringUtils;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
-import static com.querydsl.core.group.GroupBy.groupBy;
-import static com.querydsl.core.group.GroupBy.set;
-
-public class ShopRepositoryImpl implements ShopRepositoryCustom {
+public class ShopRepositoryImpl extends QuerydslRepositorySupport implements ShopRepositoryCustom {
     private final JPAQueryFactory jpaQueryFactory;
 
     public ShopRepositoryImpl(JPAQueryFactory jpaQueryFactory) {
+        super(Shop.class);
         this.jpaQueryFactory = jpaQueryFactory;
     }
 
@@ -32,21 +37,57 @@ public class ShopRepositoryImpl implements ShopRepositoryCustom {
 
     @Override
     public List<Shop> findAllDynamicQueryOrderByCreatedAtDesc(String name, int pageNum, int pageSize) {
-        return jpaQueryFactory.select(QShop.shop)
-                .from(QShopTag.shopTag)
-                    .rightJoin(QShopTag.shopTag.shop, QShop.shop)
-//                        .on(QShop.shop.id.eq(QShopTag.shopTag.shop.id))
-//                        .fetchJoin()
-                    .leftJoin(QShopTag.shopTag.tag, QTag.tag)
-//                        .on(QShopTag.shopTag.tag.id.eq(QTag.tag.id))
-//                        .fetchJoin()
-                .where(searchCondition(name))
-                .groupBy(QShop.shop.id)
-//                .distinct()
-                .orderBy(QShop.shop.createdAt.desc())
-                .limit(pageSize)
-                .offset(pageNum)
-                .fetch();
+//        final var fetch = jpaQueryFactory.select(Projections.fields(ShopSearchResult.class,
+//                QShop.shop.id.as("shopId"),
+////                QShop.shop.thumbnail.as("thumbnail"),
+//                QShop.shop.name.as("name"),
+//                QShop.shop.url.as("url"),
+//                QShop.shop.overview.as("overview"),
+//                QShop.shop.safeAt.as("safeAt"),
+//                QShop.shop.safeLevel.as("shopSafeLevel")))
+////                QTag.tag.name.as("tagName"),
+////                QTag.tag.icon.as("tagIcon")))
+        final var fetch = from(QShopTag.shopTag)
+
+                .innerJoin(QShopTag.shopTag.tag, QTag.tag)
+                                .fetchJoin()
+                .rightJoin(QShopTag.shopTag.shop, QShop.shop)
+
+                .fetchJoin()
+//                .join(QShop.shop).on(QShopTag.shopTag.shop.id.eq(QShop.shop.id))
+//                .join(QTag.tag).on(QShopTag.shopTag.tag.id.eq(QTag.tag.id))
+            .where(deleteCondition())
+            .where(searchCondition(name))
+//            .select(QShop.shop)
+            .groupBy(QShop.shop.id)
+            .orderBy(QShop.shop.createdAt.desc())
+            .limit(pageSize)
+            .offset(pageNum)
+            .fetch();
+//        return fetch;
+        return fetch.stream()
+                .filter(Objects::nonNull)
+                .peek(s -> System.out.println(s.getShop() + ", " + s.getTag()))
+                .map(ShopTag::getShop)
+                .collect(Collectors.toList());
+
+//        fetch.stream()
+//                .map()
+
+//        return fetch;
+//        return fetch.stream()
+//                .map(t -> {
+//                    final var temp = t.get(0, Shop.class);
+//                    assert temp != null;
+//                    temp.addTag(t.get(1, Tag.class));
+//                    return temp;
+//                })
+//                .collect(Collectors.toList());
+    }
+
+    private BooleanExpression deleteCondition() {
+        return QShop.shop.deletedAt.isNull()
+                .and(QTag.tag.deletedAt.isNull());
     }
 
     private BooleanExpression searchCondition(String name) {
